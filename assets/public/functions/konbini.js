eccc9dcd3bdba08d3cab93c75151533ba5067fce
AnimeBytes <staff@animebytes.tv>
2019-01-01 19:50:08 +0100

    update konbini.js

--- assets/public/functions/konbini.js
+++ assets/public/functions/konbini.js
@@ -9,17 +9,12 @@ $j(document).ready(function() {
 
 function calculateTax(amount) {
     var sum = amount.value;
-    var formatter = new Intl.NumberFormat('en-US', {
-        style: 'decimal',
-        minimumFractionDigits: 2
-    });
 
     if (isNaN(sum) || !sum) {
         $j(".taxcalc").hide();
     } else {
-        var tax = (formatter.format(parseFloat(sum) * (konbiniSendTax / 100)));
-        var total = (formatter.format(parseFloat(sum) * ((konbiniSendTax / 100) + 1)));
-        sum = formatter.format(parseFloat(sum));
+        var tax = Math.floor(sum * (konbiniSendTax / 100))
+        var total = Math.floor(sum * ((konbiniSendTax / 100) + 1))
         $j("#sum").text(sum);
         $j("#tax").text(tax);
         $j("#total").text(total);
